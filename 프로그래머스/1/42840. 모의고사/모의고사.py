af090faa712e7915dc865b5c1b8ff84d5431864e
GyungTaek <90082748+okt03149@users.noreply.github.com>
2023-11-21 15:23:29 +0900

    [level 1] Title: 모의고사, Time: 3.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,20 @@
 def solution(answers):
-    answer = []
-    #초기화
-    one = 0
-    two = 0
-    three = 0
-    #정답 배열 생성 
-    two_arr = [1,3,4,5]
-    three_arr = [3,1,2,4,5]
+    answer = [0,0,0]
+    sol_a = [1,2,3,4,5]
+    sol_b = [2,1,2,3,2,4,2,5]
+    sol_c = [3,3,1,1,2,2,4,4,5,5]
+    
+    
     for i,ans in enumerate(answers):
-        if i%5 + 1 == ans:
-            one += 1
-        if (i%2 == 0 and ans == 2) or (i%2 != 0 and two_arr[(i//2) %4] == ans):
-            two += 1
-        if (three_arr[(i//2)%5] == ans):
-            three +=1
-    max_score = max(one,two,three)
-    if one == max_score:
-        answer.append(1)
-    if two == max_score:
-        answer.append(2)
-    if three == max_score:
-        answer.append(3)
-    return answer
+        if ans == sol_a[i%len(sol_a)]:
+            answer[0] += 1
+        if ans == sol_b[i%len(sol_b)]:
+            answer[1] += 1
+        if ans == sol_c[i%len(sol_c)]:
+            answer[2] +=1
+    a = []
+    max_score = max(answer)
+    for i,n in enumerate(answer):
+        if n == max_score:
+            a.append(i+1)
+    return a

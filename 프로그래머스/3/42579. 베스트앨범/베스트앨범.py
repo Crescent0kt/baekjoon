a3e8e9ac2b78334145a2bf221798f43c0a74b7bc
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-11-29 16:46:17 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,34 +1,32 @@
-
 def solution(genres, plays):
     answer = []
-    dict = {}
-    #song = 고유번호 ->[재생횟수, 장르]
-    song = []
-    for i, genre in enumerate(genres):
-        if genre in dict:
-            dict[genre] +=plays[i]
-        else:
-            dict[genre] = plays[i]
-        song.append([plays[i],genre])
-
-    #sort_dict = 아이템들을 play값으로 내림차순으로 정렬, 이게 장르 순서 
-    sort_dict = sorted(dict.items(),key=lambda x:x[1],reverse=True)
+    dict_genre = {}
+    dict_count = {}
     
-    #제일 큰 sort_dict부터 끝까지 => 모든장르에서
-    for i in range(len(sort_dict)):
-        tmparr = []
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
         
-        #j == 번호 element == [plays[i],genre]
-        #현재 song의 장르가 지금 보는 장르와 같으면
-        #재생수, 고유번호 배열에 넣음
-        for j,element in enumerate(song):
-            if element[1] == sort_dict[i][0]:
-                tmparr.append([song[j][0],j])
-                
-        if len(tmparr) <= 1:
-            answer.append(tmparr.pop()[1])
+        if genre in dict_genre:
+            dict_genre[genre].append((-play,i))
+            dict_count[genre] += play
         else:
-            tmparr.sort(key = lambda x: (x[0], -x[1]))
-            answer.append(tmparr.pop()[1])
-            answer.append(tmparr.pop()[1])
+            dict_genre[genre] = []
+            dict_genre[genre].append((-play,i))
+            dict_count[genre] = play
+        
+    genre_rank = []
+    for key in dict_count.keys():
+        genre_rank.append([dict_count[key],key])
+    genre_rank.sort(reverse = True)
+        
+        
+    for _,genre in genre_rank:
+        music_list = dict_genre[genre]
+        music_list.sort(key = lambda x:x[0])
+        if len(music_list) == 1:
+            answer.append(music_list[0][1])
+        else:
+            answer.append(music_list[0][1])
+            answer.append(music_list[1][1])
     return answer

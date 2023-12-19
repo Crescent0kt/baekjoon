85f134dbd00794cd06722cee0015216befe25af1
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-12-19 16:02:56 +0900

    [level 2] Title: N-Queen, Time: 2285.42 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,11 @@
+def solution(n):
+    def dfs(queens, xy_dif, xy_sum):
+        p = len(queens)
+        if p == n:
+            return 1
+        result = 0
+        for q in range(n):
+            if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
+                result += dfs(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
+        return result
+    return dfs([], [], [])

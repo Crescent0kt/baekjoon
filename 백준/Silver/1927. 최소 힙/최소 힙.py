09b3fc8fc131b55ee30004b6aa963731a6a5529d
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-12-22 12:01:55 +0900

    [Silver II] Title: 최소 힙, Time: 192 ms, Memory: 114136 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+N = int(sys.stdin.readline())
+
+min_heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not min_heap:
+            print(0)
+        else:
+            print(heapq.heappop(min_heap))
+    else:
+        heapq.heappush(min_heap,x)

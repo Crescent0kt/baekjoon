104e46053ec18d315e8ca998beb07e5e9959b7cb
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-12-25 11:13:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 572 ms, Memory: 46740 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,12 +1,42 @@
 import sys
 
 N = int(sys.stdin.readline())
+
 arr = []
 for _ in range(N):
-    arr.append(list(map(int, input().split())))
+    x,y = map(int,sys.stdin.readline().split())
+    arr.append((x,y))
 
-arr.sort(key = lambda x: (x[0],x[1]))
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-for a in arr:
-    print(a[0],a[1])
+    #1쪼갬
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
 
+    #2합침
+    cur_left = 0
+    cur_right = 0
+    new_arr = []
+    while cur_left <len(left) and cur_right< len(right):
+        if left[cur_left][0] > right[cur_right][0] or (left[cur_left][0] == right[cur_right][0] and left[cur_left][1] > right[cur_right][1]):
+            new_arr.append(right[cur_right])
+            cur_right+= 1
+        else:
+            new_arr.append(left[cur_left])
+            cur_left += 1
+    else:
+        while cur_left < len(left):
+            new_arr.append(left[cur_left])
+            cur_left += 1
+        while cur_right< len(right):
+            new_arr.append(right[cur_right])
+            cur_right += 1
+
+    return new_arr
+
+new = merge_sort(arr)
+for x,y in new:
+    print(x,y)

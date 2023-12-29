7fc37bcea32a9b71c2e7d82a4f3dc2c4325841c9
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-12-29 09:51:49 +0900

    [Gold IV] Title: 최단경로, Time: 728 ms, Memory: 70200 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,29 +1,55 @@
+"""
+문제
+방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의
+최단 경로를 구하는 프로그램을 작성하시오.
+단, 모든 간선의 가중치는 10 이하의 자연수이다.
+
+입력
+첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다.
+(1 ≤ V ≤ 20,000, 1 ≤ E ≤ 300,000)
+모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다.
+둘째 줄에는 시작 정점의 번호 K(1 ≤ K ≤ V)가 주어진다.
+셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다.
+이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다. u와 v는 서로 다르며 w는 10 이하의 자연수이다.
+서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
+
+출력
+첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다.
+시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
+"""
 
 import sys
 import heapq
-INF = sys.maxsize
+from collections import defaultdict
+INF = int(1e9)
 V,E = map(int,sys.stdin.readline().split())
-#시작정점
 K = int(sys.stdin.readline())
-graph = list([] for _ in range(V+1))
+
+graph = defaultdict(list)
+
 for _ in range(E):
     u,v,w = map(int,sys.stdin.readline().split())
-    graph[u].append([v,w])
+    graph[u].append((v,w))
 
-pq = []
-heapq.heappush(pq,[0,K])
-dist = [INF] * (V+1)
-dist[K] = 0
+visited = [INF] * (V+1)
 
-while pq:
-    cur_cost, cur_node = heapq.heappop(pq)
+q = [(0,K)]
+visited[K] = 0
+while q:
+    val, node = heapq.heappop(q)
+
+    if visited[node] < val:
+        continue
     
-    if cur_cost > dist[cur_node]: continue
-    for next_node, next_cost in graph[cur_node]:
-        if next_cost + cur_cost < dist[next_node]:
-            dist[next_node] = next_cost + cur_cost
-            heapq.heappush(pq,[next_cost + cur_cost,next_node])
-
-for i in range(1,len(dist)):
-    if dist[i] >= INF: print("INF")
-    else: print(dist[i])
+    for next_node,next_val in graph[node]:
+        next_val += val
+        if visited[next_node] > next_val:
+            visited[next_node] = next_val
+            heapq.heappush(q,(next_val, next_node))
+for i in visited[1:]:
+    if(i == INF):
+        print("INF")
+    else:
+        print(i)
+    
+    

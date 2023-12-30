f2e4cd7fd9e5d8459b1d5b360d602e4bbc077f56
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2023-12-30 10:52:46 +0900

    [Silver I] Title: 정수 삼각형, Time: 144 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,11 @@
 import sys
-
 n = int(sys.stdin.readline())
 
-D = [0] * (n+1)
-
-#D[0]은 비울거기때문에 D배열 접근시 +1
+arr = [0]* n
 for i in range(n):
-    arr = list(map(int, sys.stdin.readline().split()))
-    tmp = D[:]
-    for i, num in enumerate(arr):
-        D[i+1] = max(tmp[i],tmp[i+1]) + num
+    nums = list(map(int,sys.stdin.readline().split()))
+    for i in range(len(nums)-1,0,-1):
+        arr[i] = max(arr[i-1],arr[i]) + nums[i]
+    arr[0] += nums[0]
 
-print(max(D))
+print(max(arr))

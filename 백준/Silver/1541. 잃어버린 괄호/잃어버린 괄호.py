a8d8f73b90974a57ecbf082099b2c2df7d7a5532
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-10 09:20:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,44 +1,18 @@
-"""
-식은 0 - 9 + , - 로 이루어져 있음
-괄호를 적절히 쳐서 식의 값을 최소로 만드는 프로그램을 작성하시오
-처음과 마지막은 무조건 숫자이다.
-연속해서 두 연산자가 나타나지 않고, 5자리 보다 많은 숫자는 없음, 수는 0으로 시작할 수 있고, 식의 길이는 50보다 작거나 같다.
-"""
-
 import sys
 
 str = sys.stdin.readline().rstrip()
+
+num = 0
 ans = 0
-is_minus = False
-tmp_str = ""
+if "-" in str:
+    plus, minus = str.split("-",1)[0], str.split("-",1)[1]
+    p = sum(map(int,plus.split("+")))
 
-for s in str:
-    if s == "+":
-
-        if is_minus:
-            ans -= int(tmp_str)
-            tmp_str = ""
-        else:
-            ans += int(tmp_str)
-            tmp_str = ""
-
-    elif s == "-":
-
-        if not is_minus:
-            is_minus = True
-            ans+= int(tmp_str)
-            tmp_str = ""
-        else:
-            ans -= int(tmp_str)
-            tmp_str = ""
-    else:
-        tmp_str += s
+    minus = minus.replace("-", "+")
+    m = sum(map(int,minus.split("+")))
 else:
-    if is_minus:
-        ans -= int(tmp_str)
+    plus, minus = str, ""
+    p = sum(map(int,plus.split("+")))
+    m = 0
 
-    else:
-        ans += int(tmp_str)
-
-
-print(ans)
+print(p-m)

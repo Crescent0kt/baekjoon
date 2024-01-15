d0d65a82ac83cd39b58a98827425abab52859073
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-15 09:25:30 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,16 +1,20 @@
+#현재 배열 값(현재 계단에서의 최고점)
+#현재 계단에서의 최고값 -> 뛰어서 한칸전에서 나까지오기 or 뛰어서 나한테오기
 import sys
+N = int(sys.stdin.readline())
 
-n = int(sys.stdin.readline())
+stair = [0]
+for _ in range(N):
+    stair.append(int(sys.stdin.readline()))
 
-s = list(int(sys.stdin.readline()) for _ in range(n))
+if len(stair) == 2:
+    print(stair[-1])
+    exit()
+ans = [0] * (N+1)
+ans[1] = stair[1]
+ans[2] = stair[1] + stair[2]
 
-if n <= 2:
-    print(sum(s))
-else:
-    dp = [0] * n
-    dp[0] = s[0]
-    dp[1] = s[1] + s[0]
+for i in range(3,N+1):
+    ans[i] = stair[i] + max(ans[i-2], ans[i-3]+stair[i-1])
 
-    for i in range(2,n):
-        dp[i] = max(dp[i-2] + s[i], dp[i-3]+s[i-1]+s[i])
-    print(dp[n-1])
+print(ans[-1])

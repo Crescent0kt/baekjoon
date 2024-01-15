7a39eeaa9b60faebf1eff6e29ddce6e8ace7e6ca
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-15 09:06:22 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,16 +1,13 @@
 import sys
-import heapq
 
-N = int(sys.stdin.readline())
-q=  list(map(int,sys.stdin.readline().split()))
-heapq.heapify(q)
+N =int(sys.stdin.readline())
+arr = list(map(int,sys.stdin.readline().split()))
 
+arr.sort()
 ans = 0
-aa = 0
+time =0
 for i in range(N):
-    aa = aa + heapq.heappop(q)
-    ans = ans + aa
+    time = time+arr[i]
+    ans = ans + time
 
 print(ans)
-
-    

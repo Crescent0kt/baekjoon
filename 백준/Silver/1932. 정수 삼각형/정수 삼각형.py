13ec33205cecfc02bd173be1bc8e7fc0c0062a2f
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-17 09:29:00 +0900

    [Silver I] Title: 정수 삼각형, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,11 +1,16 @@
 import sys
-n = int(sys.stdin.readline())
 
-arr = [0]* n
-for i in range(n):
-    nums = list(map(int,sys.stdin.readline().split()))
-    for i in range(len(nums)-1,0,-1):
-        arr[i] = max(arr[i-1],arr[i]) + nums[i]
-    arr[0] += nums[0]
+N = int(sys.stdin.readline())
+
+arr = [0] * N
+
+for i in range(N):
+    tmpArr = arr[:]
+    numList = list(map(int,sys.stdin.readline().split()))
+
+    arr[0] += numList[0]
+    for j in range(1,len(numList)):
+        arr[j] = max(tmpArr[j-1], tmpArr[j]) + numList[j]
+
 
 print(max(arr))

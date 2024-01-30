e5a27a65d781f04d39d62254c09b9a199d008529
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-01-31 08:59:36 +0900

    [level 2] Title: 타겟 넘버, Time: 4.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,14 @@
-from collections import deque
 def solution(numbers, target):
     answer = 0
-
-    dq = deque()
-    dq.append(numbers[0])
-    dq.append(-numbers[0])
-    for i in range(1,len(numbers)):
-        #홀짝
-        count = 2**i
-        while count != 0:
-            num = dq.popleft()
-            dq.append(num + numbers[i])
-            dq.append(num - numbers[i])
-            count -= 1
-    for j in dq:
-        if j == target:
-            answer+=1
-    
+    stack = [(numbers[0],1),(-numbers[0],1)]
+    while stack:
+        num, co = stack.pop()
+        if co == len(numbers):
+            if num == target:
+                answer +=1
+            continue
+        
+        stack.append((num+numbers[co],co+1))
+        stack.append((num-numbers[co],co+1))
+                    
     return answer

f9434c685fafbf19f9966d080870d5ad2bcaf83c
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-02-02 10:02:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 556 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,29 +1,28 @@
 import sys
-#k개의 랜선 -> n개로 만들고 싶어요
-n, c = map(int, sys.stdin.readline().split())
+N, C = map(int,sys.stdin.readline().split())
 
-house = []
-for _ in range(n):
-    house.append(int(sys.stdin.readline()))
+houses = []
+for i in range(N):
+    houses.append(int(sys.stdin.readline()))
 
-house.sort()
+houses.sort()
 
-#공유기 최소 거리 1, 최대 거리 max - min
-l, r = 1, house[-1] - house[0]
-ans = 0
+l, r = 1, houses[-1] - houses[0]
+result = 0
 
 while l<=r:
     mid = (l+r)//2
     count = 1
     a = 0
-    for i in range(1,n):
-        if house[i] - house[a]>= mid:
+
+    for i in range(1,N):
+        if houses[i] - houses[a]>= mid:
             count +=1
             a = i
 
-    if count>=c:
-        ans = max(ans, mid)
+    if count >= C:
+        result = max(result, mid)
         l = mid+1
     else:
-        r = mid-1
-print(ans)
+        r = mid -1
+print(result)

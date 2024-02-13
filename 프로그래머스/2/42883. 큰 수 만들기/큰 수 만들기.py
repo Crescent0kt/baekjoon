64564a2c29c6b24823504e9cb9ed2618a57c6f9a
Crescent0kt <152241642+Crescent0kt@users.noreply.github.com>
2024-02-13 09:29:51 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,18 +1,24 @@
 def solution(number, k):
-    answer = ''
-    stack = []
-    numbers = list(map(int,number))
     
-    for tmp in numbers:
-        while k!=0 and len(stack) !=0:
-            if tmp > stack[-1]:
-                stack.pop()
+    answer = [number[0]]
+    cursor = 1
+    while k >0 and cursor <len(number) :
+        while k>0 and len(answer) != 0:
+            if number[cursor] > answer[-1]:
+                answer.pop()
                 k -= 1
             else:
                 break
-        stack.append(tmp)
-    while k != 0:
-        k -= 1
-        stack.pop()
-    answer += ''.join(str(_) for _ in stack)
-    return answer
+        answer.append(number[cursor])
+        cursor += 1
+        
+        if cursor == len(number):
+            while k>0:
+                answer.pop()
+                k -= 1
+        elif k == 0:
+            while cursor<len(number):
+                answer.append(number[cursor])
+                cursor+= 1
+        
+    return ''.join(answer)

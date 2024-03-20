b852a7000cfc7d6a46f89d8d3f46212746cb3250
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-20 19:25:27 +0900

    [level 2] Title: 주식가격, Time: 14.90 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,19 @@
+from collections import deque
 def solution(prices):
-    answer = [0] * len(prices)
-    price_arr = []
-    
-    for i in range(len(prices)):
-        while i != 0 and len(price_arr)!= 0:
-            price = price_arr[-1]
-            if price[0] <= prices[i]:
-                break
+    answer = deque()
+    arr = []
+    for i,p in enumerate(prices):
+        while arr:
+            pp, ii = arr[-1]
+            if pp > p:
+                arr.pop()
+                prices[ii] = i-ii
             else:
-                price_arr.pop()
-                answer[price[1]] = i-price[1]
-        price_arr.append([prices[i],i])
-    else:
-        while len(price_arr)!=0:
-            price = price_arr.pop()
-            answer[price[1]] = len(prices)-1-price[1]
-    return answer
+                break
+        arr.append((p,i))
+    
+    while arr:
+        p,i = arr.pop()
+        prices[i] = len(prices)-1-i
+        
+    return prices

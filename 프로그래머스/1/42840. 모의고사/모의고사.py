30f528a7a865fef2aa0dd0f4505879072b40769e
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-21 14:14:44 +0900

    [level 1] Title: 모의고사, Time: 3.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,25 @@
 def solution(answers):
-    answer = [0,0,0]
-    sol_a = [1,2,3,4,5]
-    sol_b = [2,1,2,3,2,4,2,5]
-    sol_c = [3,3,1,1,2,2,4,4,5,5]
-    
-    
+    answer = []
+    loser1 = [1, 2, 3, 4, 5]
+    loser2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    loser3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score1 = 0
+    score2 = 0
+    score3 = 0
     for i,ans in enumerate(answers):
-        if ans == sol_a[i%len(sol_a)]:
-            answer[0] += 1
-        if ans == sol_b[i%len(sol_b)]:
-            answer[1] += 1
-        if ans == sol_c[i%len(sol_c)]:
-            answer[2] +=1
-    a = []
-    max_score = max(answer)
-    for i,n in enumerate(answer):
-        if n == max_score:
-            a.append(i+1)
-    return a
+        if ans == loser1[i%len(loser1)]:
+            score1 += 1
+        if ans == loser2[i%len(loser2)]:
+            score2 += 1
+        if ans == loser3[i%len(loser3)]:
+            score3 += 1
+    
+    max_score = max(score1,score2,score3)
+    if max_score == score1:
+        answer.append(1)
+    if max_score == score2:
+        answer.append(2)
+    if max_score == score3:
+        answer.append(3)
+           
+    return answer

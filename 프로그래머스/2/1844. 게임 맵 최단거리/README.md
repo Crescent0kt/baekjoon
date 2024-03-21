1d249b48a8282182a136912fd725012bc79e7b0d
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-21 12:44:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.43 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 6.99 ms
+메모리: 10.4 MB, 시간: 6.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 12:7:47
+2024년 03월 21일 12:44:52
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,26 @@
-from collections import deque
-import sys
-y = [0,-1,0,1]
-x = [-1,0,1,0]
-inf = sys.maxsize
-
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    visited = list([inf] * m for _ in range(n))
-    
-    dq = deque()
-    dq.append((0,0))
-    visited[0][0] = 1
-    
-    while len(dq) != 0:
-        
-        uy,ux = dq.popleft()
-        
+    dx = [0,-1,0,1]
+    dy = [1,0,-1,0]
+    inf = int(10e9)
+
+    N = len(maps)
+    M = len(maps[0])
+    board = [[inf] * M for _ in range(N)]
+    arr = [(0,0,1)]
+    while arr:
+        x,y,n = arr.pop(0)
+        if board[y][x] <= n:
+            continue
+        board[y][x] = n
         for i in range(4):
-            dx = ux+x[i]
-            dy = uy+y[i]
-            if 0<=dx<m and 0<=dy<n and maps[dy][dx] != 0:
-                if visited[dy][dx] > visited[uy][ux] +1:
-                    visited[dy][dx] = visited[uy][ux] +1
-                    dq.append((dy,dx))
-    answer = visited[n-1][m-1]
-    if answer == inf: answer = -1 
-    return answer
+            cx = dx[i] + x
+            cy = dy[i] + y
+            if M> cx >=0 and N>cy >=0  and maps[cy][cx] != 0:
+                if board[cy][cx] > n+1:
+                    arr.append((cx,cy,n+1))
+
+    if board[N-1][M-1] == inf:
+        return -1
+    else:
+        return board[N-1][M-1]
+    

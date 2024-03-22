a0687b9641e620181cfe39cd09aafe62edd4f806
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-22 13:09:43 +0900

    [level 2] Title: 구명보트, Time: 7.98 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,21 +1,18 @@
 def solution(people, limit):
     answer = 0
-    num_people = len(people)
     people.sort()
-    cursor_l = -1
-    cursor_r = len(people)-1
+    l = 0
+    r = len(people) - 1
+    while l<r:
+        if people[l] + people[r] <= limit:
+            answer += 1
+            l += 1
+            r -= 1
+        else:
+            answer += 1
+            r -= 1
+    else:
+        if l == r:
+            answer += 1
     
-    while cursor_l != cursor_r:
-        cursor_l += 1
-        answer += 1
-        a = people[cursor_l]
-        while cursor_l != cursor_r:
-            if people[cursor_r] + a <= limit:
-                cursor_r -= 1
-                num_people -= 1
-                break
-            cursor_r -= 1
-        num_people -= 1
-        
-    answer += num_people
     return answer

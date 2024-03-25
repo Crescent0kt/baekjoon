05f15cc3da737b8a648de313d388af156382cd84
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-25 12:50:14 +0900

    [level 2] Title: 전화번호 목록, Time: 719.86 ms, Memory: 231 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,15 +1,33 @@
+from collections import defaultdict
 def solution(phone_book):
+    dic = defaultdict(dict)
+    for phone in phone_book:
+        phonedict = dic
+        #첫 값 부터 없으면 그대로 쭉 만들기
+        if phone[0] not in phonedict:
+            for i in range(len(phone)):
+                phonedict[phone[i]] = {}
+                phonedict = phonedict[phone[i]]
+        else:
+            # 첫 값이 있는 경우
+            check = False
+            phonedict = phonedict[phone[0]]
 
-    #딕셔너리에 저장
-    dict = {}
-    for phone in phone_book:
-        dict[phone] = 1
-    
-    for phone in phone_book:
-        s = ""
-        #핸드폰 번호 각자리
-        for phone_s in phone:
-            s += phone_s
-            if s in dict and s != phone:
-                return False
+            for i in range(1,len(phone)):
+                if check:
+                    phonedict[phone[i]] = {}
+                    phonedict = phonedict[phone[i]]
+                else:   
+                    if phone[i] in phonedict:    
+                        phonedict = phonedict[phone[i]]
+                    else:
+                        if not phonedict:
+                            return False
+                        else:
+                            check = True
+                            phonedict[phone[i]] = {}
+                            phonedict = phonedict[phone[i]]
+            else:   
+                if not check:
+                    return False
     return True

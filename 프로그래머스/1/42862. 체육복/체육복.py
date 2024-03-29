b2950d07260d9277d27927e7d3c62c242afe81e6
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-03-29 12:18:08 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,16 +1,21 @@
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-
-    for i in reserve[:]:
-        if i in lost:
-            reserve.remove(i)
-            lost.remove(i)
-
-    for i in reserve:
-        if i-1 in lost:
-            lost.remove(i-1)
-        elif i+1 in lost:
-            lost.remove(i+1)
+    answer = 0
+    arr = [1] * (n+1)
+    for l in lost:
+        arr[l] -= 1
     
-    return n-len(lost)
+    for r in reserve:
+        arr[r] += 1
+    
+    for i in range(1,n+1):
+        if arr[i] == 2:
+            if arr[i-1] == 0:
+                arr[i] -= 1
+                arr[i-1] += 1
+            elif i != n and arr[i+1] == 0:
+                arr[i] -= 1
+                arr[i+1] += 1
+    for i in range(1, n+1):
+        if arr[i] > 0:
+            answer += 1
+    return answer

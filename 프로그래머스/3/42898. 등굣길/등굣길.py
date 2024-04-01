8ebfefea982b45a43aff845c4e59b177f5f7de3a
Gyungtaek Oh <152241642+Crescent0kt@users.noreply.github.com>
2024-04-01 14:37:12 +0900

    [level 3] Title: 등굣길, Time: 1.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,27 +1,28 @@
 def solution(m, n, puddles):
     answer = 0
+
+    board = [[0] * (m) for _ in range(n)]
     
-    board = list(([0] * m) for _ in range(n))
     for x,y in puddles:
         board[y-1][x-1] = -1
     
-    board[0][0] = 1
-    
-    for i in range(1,m):
-        if board[0][i] != -1:
-            board[0][i] = board[0][i-1]
-
-    for i in range(1,n):
-         if board[i][0] != -1:
-            board[i][0] = board[i-1][0]
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == -1:
+                board[i][j] = 0
+                continue
+                
+            if i == 0 and j == 0:
+                board[i][j] = 1
+                continue
             
+            if i == 0:
+                board[i][j] = board[i][j-1]
             
-    for i in range(1,n):
-        for j in range(1,m):
-            if board[i][j] != -1:
-                if board[i][j-1] != -1:
-                    board[i][j] += board[i][j-1]
-                if board[i-1][j] != -1:
-                    board[i][j] += board[i-1][j]
+            elif j == 0:
+                board[i][j] = board[i-1][j]
+                
+            else:
+                board[i][j] = board[i-1][j] + board[i][j-1]
             
     return board[n-1][m-1]%1000000007
